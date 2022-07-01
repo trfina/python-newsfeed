8029e228b9c268e368b24770b7c98514b4076c03
Tammy Fina <finatammy@gmail.com>
2022-07-01 13:18:01 -0400

    homepage route created and tested

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app.routes import home
 from flask import Flask
 def create_app(test_config=None):
     # set up app config
@@ -11,4 +12,7 @@ def create_app(test_config=None):
     def hello():
         return 'hello beautiful world'
 
+    #register routes
+    app.register_blueprint(home)
+    
     return app
